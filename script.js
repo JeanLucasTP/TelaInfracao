3609d5452703ea280a0a7941eacb225b59dfc399
jeanzeraa <30481436+jeanzeraa@users.noreply.github.com>
2017-09-13 19:26:40 -0300

    aep

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//open on ready function
+$(document).ready(function(e) {
+	
+	
+	$( "#selecione_data" ).datepicker();
+	
+	$("#buttons button").css({
+		'border':'#000 solid 2px',
+		'background-color':'transparent'
+	})
+	
+});
+
+	
